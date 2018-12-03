6961b38979b4e18ef74e83652d83714b9f704092
Zifan Jiang <zjian30@cnode0.clus.cci.emory.edu>
2018-12-03 16:14:22 -0500

    change filepath: absolute path on cluster

--- visualize.py
+++ visualize.py
@@ -3,6 +3,8 @@ visualize results for test image
 """
 
 import numpy as np
+import matplotlib as mpl
+mpl.use('Agg')
 import matplotlib.pyplot as plt
 from PIL import Image
 import torch
@@ -16,6 +18,7 @@ from skimage import io
 from skimage.transform import resize
 from models import *
 
+dir_path = '/home/zjian30/test/Facial-Expression-Recognition.Pytorch'
 cut_size = 44
 
 transform_test = transforms.Compose([
@@ -26,7 +29,7 @@ transform_test = transforms.Compose([
 def rgb2gray(rgb):
     return np.dot(rgb[...,:3], [0.299, 0.587, 0.114])
 
-raw_img = io.imread('images/1.jpg')
+raw_img = io.imread(dir_path + '/images/1.jpg')
 gray = rgb2gray(raw_img)
 gray = resize(gray, (48,48), mode='symmetric').astype(np.uint8)
 
@@ -39,7 +42,7 @@ inputs = transform_test(img)
 class_names = ['Angry', 'Disgust', 'Fear', 'Happy', 'Sad', 'Surprise', 'Neutral']
 
 net = VGG('VGG19')
-checkpoint = torch.load(os.path.join('FER2013_VGG19', 'PrivateTest_model.t7'))
+checkpoint = torch.load(dir_path + '/FER2013_VGG19' + '/PrivateTest_model.t7')
 net.load_state_dict(checkpoint['net'])
 net.cuda()
 net.eval()
@@ -79,7 +82,8 @@ plt.ylabel(" Classification Score ",fontsize=16)
 plt.xticks(ind, class_names, rotation=45, fontsize=14)
 
 axes=plt.subplot(1, 3, 3)
-emojis_img = io.imread('images/emojis/%s.png' % str(class_names[int(predicted.cpu().numpy())]))
+# use absolute path
+emojis_img = io.imread(dir_path + os.path.join('/images/emojis/%s.png' % str(class_names[int(predicted.cpu().numpy())])))
 plt.imshow(emojis_img)
 plt.xlabel('Emoji Expression', fontsize=16)
 axes.set_xticks([])
@@ -88,7 +92,7 @@ plt.tight_layout()
 # show emojis
 
 #plt.show()
-plt.savefig(os.path.join('images/results/l.png'))
+plt.savefig(dir_path + '/images/results/l.png')
 plt.close()
 
 print("The Expression is %s" %str(class_names[int(predicted.cpu().numpy())]))
